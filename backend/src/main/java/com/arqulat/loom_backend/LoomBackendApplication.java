c8eeff3e4b0e185ba8a55299e38bea70acd1c175
ganesh714 <evvganesh1@gmail.com>
2026-06-16 23:45:48 +0530

    refactor(backend): reset backend directory with spring initializer and backup old to backend-old

---
+++ backend/src/main/java/com/arqulat/loom_backend/LoomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arqulat.loom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoomBackendApplication.class, args);
+	}
+
+}
