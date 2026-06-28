68075fa81c9616ba6e40ed9f8fda9075d0b2d98d
ganesh714 <evvganesh1@gmail.com>
2026-06-28 19:10:22 +0530

    added root url end point

--- backend/src/main/java/com/arqulat/loom_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/arqulat/loom_backend/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .exceptionHandling(customizer -> customizer
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/error").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll())
                 .sessionManagement(session -> session
